998723dee78d6fb98822fd0665bb44d8c185a478
AJIL-PAUL <ajilpaul.268@gmail.com>
2021-10-14 20:48:13 +0530

    Added ability to assign task to a user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    if @task.save
-      render status: :ok, json: { notice: t("successfully_created") }
+    task = Task.new(task_params)
+    if task.save
+      render status: :ok, json: { notice: "Task was successfully created" }
     else
       errors = task.errors.full_messages.to_sentence
       render status: :unprocessable_entity, json: { error: errors }
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    render status: :ok, json: { task: @task }
+    render status: :ok, json: { task: @task, assigned_user: @task.assigned_user }
   end
 
   def update
@@ -42,14 +42,14 @@ class TasksController < ApplicationController
 
   private
 
+    def task_params
+      params.require(:task).permit(:title, :assigned_user_id)
+    end
+
     def load_task
       @task = Task.find_by(slug: params[:slug])
       unless @task
         render status: :not_found, json: { error: t("task.not_found") }
       end
-    end
-
-    def task_params
-      params.require(:task).permit(:title)
     end
 end
